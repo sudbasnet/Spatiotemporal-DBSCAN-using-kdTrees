4a34a1a9d95937b760f7b3eb706384233982b3e0
Sudeep Basnet <sudbasnet10@gmail.com>
2018-05-10 13:35:05 -0500

    bounding box defined and traverse function defined

--- kdTree.py
+++ kdTree.py
@@ -2,25 +2,21 @@ import heapq
 import itertools
 import operator
 import math
+import copy
+import queue
 from collections import deque
 from functools import wraps
 import fileinput
 from haversine import haversine
-from datetime import datetime
-import copy
-import queue
+from datetime import datetime, timedelta
 
 __author__ = 'Sudeep Basnet'
 
-nearest = []
-points = []
-# has uniqueid, longitude, latitude and event date
 
-
-def circle_distance(node, target, miles=False):
-    p1 = (node.coords[0], node.coords[1])
-    p2 = (target[0], target[1])
-    return haversine.haversine(p1, p2, miles)
+# def circle_distance(node, target, miles=False):
+#     p1 = (node.coords[0], node.coords[1])
+#     p2 = (target[0], target[1])
+#     return haversine.haversine(p1, p2, miles)
 
 
 def time_distance(node, target):
@@ -48,7 +44,7 @@ def float_compare(x, y):
 
 
 def prep(file):
-    global points
+    points = []
     for line in fileinput.input(file):
         info = line.split(',')
         points.append((int(info[0]), float(info[1]), float(info[2]), int(info[3])))
@@ -59,13 +55,12 @@ def prep(file):
 
 # Generate our KDTree.  Depth starts at 0 and gets incremented as we recurse
 def generate(points, tree_depth=0):
-    if points==[]:
+    if points.count() == 0:
         return None
-    # This is either 1 or 2 since we are using a 2 dimensional space
+    # This is either 0, 1 or 2 since we are using a 3 dimensional space
     axis = tree_depth % 3 + 1
     # Sort the points by their coordinates
     p = sorted(points, key=lambda x: float(x[axis]), reverse=False)
-    # points.sort(cmp=lambda x,y: float_compare(x[axis], y[axis]))
     # Pick the middle point of the tree to start as the root.
     median = math.floor(len(p) / 2)
     # Set this node to the first value
@@ -77,73 +72,77 @@ def generate(points, tree_depth=0):
     return node
 
 
-def get_subtree(kdtree_item, n, tree_depth=0):
-    if kdtree_item.id == n.id:
-        return kdtree_item
+def get_subtree(tree, n, tree_depth=0):
+    if tree.id == n.id:
+        return tree
     axis = tree_depth % 3
-    maintree_val = 0
+    tree_val = 0
     node_val = 0
-    nextnode = copy.deepcopy(kdtree_item)
+    next_node = copy.deepcopy(tree)
     if axis < 2:
-        maintree_val = kdtree_item.coords[axis]
+        tree_val = tree.coords[axis]
         node_val = n.coords[axis]
     elif axis == 2:
-        maintree_val = kdtree_item.day
+        tree_val = tree.day
         node_val = n.day
-    if node_val < maintree_val:
-        nextnode = kdtree_item.left
-    elif node_val > maintree_val:
-        nextnode = kdtree_item.right
-    return get_subtree(nextnode, n, tree_depth+1)
+    if node_val < tree_val:
+        next_node = tree.left
+    elif node_val > tree_val:
+        next_node = tree.right
+    return get_subtree(next_node, n, tree_depth+1)
 
 
-def get_all_side_nodes(kdtree_item, side='all'):
-    q = queue.Queue()
-    side_nodes = []
-    if side == 'l':
-        q.put(kdtree_item.left)
-    elif side == 'r':
-        q.put(kdtree_item.right)
-    else:
-        q.put(kdtree_item.left)
-        q.put(kdtree_item.right)
-    while not q.empty():
-        n = q.get()
-        side_nodes.append(n.id)
-        if n.left is not None:
-            q.put(n.left)
-        if n.right is not None:
-            q.put(n.right)
-    return side_nodes
+# def get_all_side_nodes(kdtree_item, side='all'):
+#     q = queue.Queue()
+#     side_nodes = []
+#     if side == 'l':
+#         q.put(kdtree_item.left)
+#     elif side == 'r':
+#         q.put(kdtree_item.right)
+#     else:
+#         q.put(kdtree_item.left)
+#         q.put(kdtree_item.right)
+#     while not q.empty():
+#         n = q.get()
+#         side_nodes.append(n.id)
+#         if n.left is not None:
+#             q.put(n.left)
+#         if n.right is not None:
+#             q.put(n.right)
+#     return side_nodes
 
 
-kdTree_object = prep('events_data_write.csv')
-
-
-# def nn(kd_tree, target, istreepoint = True ):
-#     get_bbox(target)
-
-
+# Here we define a function that will return a (lon,lat) coordinate based
+# on the source-point, distance (in km) and the bearing degree (0 or 90 or 180 or 270)
 def get_coordinate(point, d, bearing=0):
-    R = 6378.1  # Radius of the Earth in km
+    radius = 6378.1  # Radius of the Earth in km
     lon1, lat1 = point
     lon1, lat1, bearing = map(math.radians, [lon1, lat1, bearing])
-    lat2 = math.asin(math.sin(lat1) * math.cos(d / R) + math.cos(lat1) * math.sin(d / R) * math.cos(bearing))
-    lon2 = lon1 + math.atan2(math.sin(bearing) * math.sin(d / R) * math.cos(lat1), math.cos(d / R) - math.sin(lat1) * math.sin(lat2))
+    lat2 = math.asin(math.sin(lat1) * math.cos(d / radius) + math.cos(lat1) * math.sin(d / radius) * math.cos(bearing))
+    lon2 = lon1 + math.atan2(math.sin(bearing) * math.sin(d / radius) * math.cos(lat1), math.cos(d / radius) -
+                             math.sin(lat1) * math.sin(lat2))
     lon2, lat2 = map(math.degrees, [lon2, lat2])
     point2 = (lon2, lat2)
     return point2
 
 
-def get_bbox(point, d, bearing=0):
+# Now we use the above function to get a bounding box
+# around our source point. distance_tuple = (spatial_distance, temporal_distance)
+def get_bbox(node, distance_tuple, bearing=0):
+    point = node.coords
+    t = datetime.strptime(str(node.day), '%Y%m%d')
+
+    d = distance_tuple[0]
+    delta_t = timedelta(days=distance_tuple[1])
+
     cross_bounding = [[point, None]]
     bounding = []
-    bounding_coords = {}
+    boundingbox = {}
     x_max = point[0]
     x_min = point[0]
     y_max = point[1]
     y_min = point[1]
-    for x in range(4):
+    for x in range(4):  # 4 different directions
         point2 = [get_coordinate(point, d, bearing), bearing]
         cross_bounding.append(point2)
         if x_max < point2[0][0]:
@@ -154,7 +153,12 @@ def get_bbox(point, d, bearing=0):
             y_max = point2[0][1]
         if y_min > point2[0][1]:
             y_min = point2[0][1]
+            # starting at 0 degrees, we add 90 degree until we get points in all 4 directions
         bearing = bearing + 90
+
+    t_min = int(datetime.strftime(t - delta_t, '%Y%m%d'))
+    t_max = int(datetime.strftime(t + delta_t, '%Y%m%d'))
+
     bb_left_above = (x_min, y_max)
     bounding.append(bb_left_above)
     bb_left_down = (x_min, y_min)
@@ -163,55 +167,48 @@ def get_bbox(point, d, bearing=0):
     bounding.append(bb_right_above)
     bb_right_below = (x_max, y_min)
     bounding.append(bb_right_below)
-    bounding_coords.update({"x_max": x_max})
-    bounding_coords.update({"x_min": x_min})
-    bounding_coords.update({"y_max": y_max})
-    bounding_coords.update({"y_min": y_min})
-    return bounding_coords
+
+    boundingbox.update({"max": (x_max, y_max, t_max)})
+    boundingbox.update({"min": (x_min, y_min, t_min)})
+    return boundingbox
 
 
-def find_neighbors(tree, input_node, distance):
-    point = input_node.coords
-    day = input_node.day
-    bounding_coords = get_bbox(point, distance)
-    x_max = bounding_coords['x_max']
-    x_min = bounding_coords['x_min']
-    y_max = bounding_coords['y_max']
-    y_min = bounding_coords['y_min']
+def is_valid(xyz, boundingbox):
+    boundingbox_max = boundingbox["max"]
+    boundingbox_min = boundingbox["min"]
+    if xyz[0] <= boundingbox_max[0] & xyz[0] >= boundingbox_min[0] & xyz[1] <= boundingbox_max[1] & xyz[2] <= \
+            boundingbox_max[2] & xyz[2] >= boundingbox_min[2]:
+        return True
+    else:
+        return False
+
+
+# now we read this bounding coordinates to traverse the dataset
+# the original tree has been passed, with the current node and bounding coordinates
+def fixed_radius_neighbors(tree, input_node, distance):
     tree_depth = 0
-    
+    boundingbox = get_bbox(input_node, distance)
+    neighbors = []
+    node_path = queue.Queue
+    node_path.put(tree)
+    while not node_path.empty():
+        current_node = node_path.get()
+        xyz = (current_node.coords[0], current_node.coords[1], tree.day)
+        if is_valid(xyz, boundingbox):
+            if haversine(current_node.coords, input_node.coords) <= distance[0] & current_node.id != input_node.id:
+                neighbor = Node(id=current_node.id, coords=current_node.coords, day=current_node.day)
+                neighbors.append(neighbor)
+        boundingbox_max = boundingbox["max"]
+        boundingbox_min = boundingbox["min"]
+        axis = tree_depth % 3
+        if boundingbox_max[axis] > xyz[axis] & boundingbox_min[axis] >= xyz[axis]:
+            node_path.put(current_node.right)
+        elif boundingbox_max[axis] <= xyz[axis] & boundingbox_min[axis] < xyz[axis]:
+            node_path.put(current_node.left)
+        elif boundingbox_max[axis] > xyz[axis] & boundingbox_min[axis] < xyz[axis]:
+            node_path.put(current_node.right)
+            node_path.put(current_node.left)
+        tree_depth = tree_depth + 1
+    return neighbors
 
-
-
-
-# def fixed_radius_neighbors(data, kdtree_of_data, eps, tree_depth=0):
-#     return_set = {}
-#     for i in range(len(data)+1):
-#         neighbors = []
-#         return_set.update({data[i][0] : []})
-#         n = Node(data[i][0], (data[i][1], data[i][2]), data[i][3])
-#         tree = get_subtree(kdTree_object, n)
-#         left_subtree(tree, )
-#         distance = (circle_distance(subtree, subtree.left) , time_distance())
-#
-#     thisnode = Node(data)
-#     d =
-#     axis = tree_depth % 3
-#
-#     maintree_val = 0
-#     node_val = 0
-#     nextnode = copy.deepcopy(kdtree_item)
-#     if nextnode.id == n.id:
-#         return nextnode
-#     if axis < 2:
-#         maintree_val = kdtree_item.coords[axis]
-#         node_val = n.coords[axis]
-#     elif axis == 2:
-#         maintree_val = kdtree_item.day
-#         node_val = n.day
-#     if node_val < maintree_val:
-#         nextnode = kdtree_item.left
-#     elif n.node_val > maintree_val:
-#         nextnode = kdtree_item.right
-#     get_subtree(nextnode, n, tree_depth + 1)
-
+kdTree_object = prep('events_data_write.csv')
